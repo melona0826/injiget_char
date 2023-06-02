c3c3b7752e4445217a7d05a7c20dbde3825a3831
Kwon Jin <kwonjin@kaist.ac.kr>
2023-06-03 04:42:13 +0900

    Initial Commit

---
+++ cam_pub/scripts/cam_pub_node.py
@@ -0,0 +1,21 @@
+
+
+import rospy
+import cv2
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+cap = cv2.VideoCapture(0)
+
+rospy.init_node("cam_pub", anonymous=True)
+img_pub = rospy.Publisher("cam_img_pub", Image, queue_size=1)
+
+bridge = CvBridge()
+
+while not rospy.is_shutdown() :
+  ret, frame = cap.read()
+  img_pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+
+
+cap.release()
