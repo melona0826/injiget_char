d2fde583ea63781d21485f5ca00a94bf2ce06512
Kwon Jin <kwonjin@kaist.ac.kr>
2023-06-04 05:21:20 +0900

    Initial Commit

--- pallet_det/script/pallet_det_node.py
+++ pallet_det/script/pallet_det_node.py
@@ -6,22 +6,32 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened() :
   print('Camera is Not oppend !')
 
-pallet_h_min = 22
-pallet_h_max = 38
+pallet_h_min = 40
+pallet_h_max = 60
 
-pallet_s_min = 90
-pallet_s_max = 255
+pallet_s_min = 17
+pallet_s_max = 100
 
-pallet_v_min = 200
-pallet_v_max = 255
+pallet_v_min = 30
+pallet_v_max = 100
+
+pallet_lower = (int(pallet_h_min/2), int(pallet_s_min*2.55), int(pallet_v_min*2.55))
+pallet_upper = (int(pallet_h_max/2), int(pallet_s_max*2.55), int(pallet_v_max*2.55))
 
 while True :
   ret, frame = cap.read()
 
-  hsv_img = cv2.imshow
+  hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+  h,s,v = cv2.split(hsv_img)
 
-  cv2.imshow('test',frame)
+  pallet_mask = cv2.inRange(hsv_img, pallet_lower, pallet_upper)
+
+  pallet_img = cv2.bitwise_and(frame, frame, mask=pallet_mask)
+
+  cv2.imshow('h', h)
+  cv2.imshow('s', s)
+  cv2.imshow('v', v)
+  cv2.imshow('test',pallet_img)
   cv2.waitKey(1)
-
 
